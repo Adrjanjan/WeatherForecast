30bdbee6181e4783b1c68511cb9663a52ce97484
Guest User <Guest@imac-10.local>
2020-03-10 15:17:49 +0100

    Lab1

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let apiLinf = "//www.metaweather.com/api/location/44418"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 

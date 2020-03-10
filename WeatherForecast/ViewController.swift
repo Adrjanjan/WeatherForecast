69d949566f5b3febfce93f1dcf7caaf8bdbcf278
Guest User <Guest@imac-10.local>
2020-03-10 14:12:12 +0100

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Guest User on 10.03.2020.
+//  Copyright © 2020 Guest User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

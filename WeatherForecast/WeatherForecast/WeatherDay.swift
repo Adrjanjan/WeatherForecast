0815abf4d1af542ac1ed6b8cd88dea4648b4cb03
Adrian Janakiewicz <33720941+Adrjanjan@users.noreply.github.com>
2020-03-11 01:08:19 +0100

    Lab1 nearly finished

---
+++ WeatherForecast/WeatherForecast/WeatherDay.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherDay.swift
+//  WeatherForecast
+//
+//  Created by Ania Wójcik on 10/03/2020.
+//  Copyright © 2020 Guest User. All rights reserved.
+//
+
+import Foundation
+
+class WeatherDay {
+    let weatherType: String
+    let minTemperature: Double
+    let maxTemperature: Double
+    let windDirection: Double
+    let windSpeed: Double
+    let airPreassure: Double
+    let precipitation: Double
+    let icon:String
+    let timeStamp:Double
+
+    init(_ json:[String:Any]){
+       self.weatherType = json["summary"] as! String
+       self.maxTemperature = json["temperatureMax"] as! Double
+       self.minTemperature = json["temperatureMin"] as! Double
+       self.windSpeed = json["windSpeed"] as! Double
+       self.windDirection = json["windBearing"] as! Double
+       self.precipitation = json["humidity"] as! Double
+       self.airPreassure = json["pressure"] as! Double
+       self.icon = json["icon"] as! String
+       self.timeStamp = json["time"] as! Double
+    }
+    
+}
